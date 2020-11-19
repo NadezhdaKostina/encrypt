54a8e3e0de8c35af6a68c06c29d72f64b2a07c9c
nadezhda <1seligselig@gmail.com>
2020-11-19 20:27:49 +0300

    until

--- encrypt.rb
+++ encrypt.rb
@@ -6,8 +6,16 @@ user_input = STDIN.gets.chomp
 puts "What encoding do you prefer: 1) MD5, 2) SHA1 or 3) SHA2. Please input 1, 2 or 3"
 encrypt = STDIN.gets.to_i
 
+until encrypt.between?(1, 3)
+  puts 'Выберите 1 или 2'
+  encrypt = STDIN.gets.to_i
+end
+
 case encrypt
-when 1 then puts Digest::MD5.hexdigest(user_input)
-when 2 then puts Digest::SHA1.hexdigest(user_input)
-when 3 then puts Digest::SHA2.hexdigest(user_input)
+when 1 then
+  puts Digest::MD5.hexdigest(user_input)
+when 2 then
+  puts Digest::SHA1.hexdigest(user_input)
+when 3 then
+  puts Digest::SHA2.hexdigest(user_input)
 end
